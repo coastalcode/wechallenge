857a5cf6f227c50de1a22beb2665f3051537c5b5
John Smalley <john.m.smalley@gmail.com>
2016-09-22 14:56:05 -0700

    added client side redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
 import App from './components/App';
 
+let store = createStore(rootReducer);
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('app')
 );
