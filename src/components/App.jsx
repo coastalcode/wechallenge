0da9b1d2689b41de3d1a3c384aa2b443f28132e0
John Smalley <john.m.smalley@gmail.com>
2016-09-22 12:06:51 -0700

    about to add nav bar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <h1>Hello World!!!</h1>
+      <div>
+        <h1>Hello World!!!</h1>
+      </div>
     )
   }
 }
