55f7549367c5faa5f9f2205196c052da6ce7e3eb
Jim Yang <jimyyang@gmail.com>
2016-09-22 14:59:48 -0400

    fixing failed test trav test

--- hello.js
+++ hello.js
@@ -1,5 +1,5 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Travis!\n') //this fails
+  res.end('Hello Travis!\n'); //this fails
 }).listen(1337, '127.0.0.1');
