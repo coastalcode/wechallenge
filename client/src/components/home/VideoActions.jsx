6c7da0748759a121c9f19e26ecaed26a7c139cd5
Jim Yang <jimyyang@gmail.com>
2016-09-26 23:45:25 -0400

    more styles for homepage components

--- client/src/components/home/VideoActions.jsx
+++ client/src/components/home/VideoActions.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 export default class VideoAction extends React.Component {
   constructor(props) {
     super(props)
+    console.log('props', this.props.points, this.props.comments)
   }
   render() {
     return(
       <div className="videoactions-container">
         <span className="videolists-flexbuffer"></span>
-        <span className="videoaction-button points">100 points</span>
-        <span className="videoaction-button comments">Comments: 99</span>
-        <span className="videoaction-button watch">Watch</span>
+        <span className="videoaction-button points">{this.props.points} points</span>
+        <span className="videoaction-button comments">{this.props.comments} comments</span>
         <span className="videoaction-button share">Share</span>
         <span className="videolists-flexbuffer"></span>
       </div>
--- client/src/components/home/VideoList.jsx
+++ client/src/components/home/VideoList.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Video from './Video';
 import YouTube from 'react-youtube';
+import VideoActions from './VideoActions';
 
 export default class VideoList extends React.Component {
   constructor(props) {
     super(props);
   }
   componentDidMount() {
-    console.log('grab list of videos from', this.props)
+    console.log('VideoList loaded')
   }
 
   render() {
@@ -16,17 +17,23 @@ export default class VideoList extends React.Component {
       width: '400'
     }
     return (
-      <div>
+      <div className="videolist-columns">
         { this.props.locale === 'state' ?
           <h1 className="home-header topLocal">Top Records In Your Area</h1>
           :
           <h1 className="home-header topGlobal">Top Records Worldwide</h1>
         }
         { this.props.videos.map((val)=>{
+          let thumbStyle = {
+            width: '80%'
+          }
           return (
-            <div className="youtube-container">
-              <span className="title-banner">{val.title}</span>
-              <img src={"http://img.youtube.com/vi/" + val.videoID + "/hqdefault.jpg"}/>
+            <div>
+              <div className="youtube-container">
+                <span className="title-banner">{val.title}</span>
+                <img style={thumbStyle} className="videolist-thumb" src={ "http://img.youtube.com/vi/" + val.videoID + "/hqdefault.jpg" }/>
+              </div>
+              <VideoActions points={val.points} comments={val.comments} />
             </div>
           )
         })}
