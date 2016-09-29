17cdc86fa9a99a564dac493d889badbadc20649c
John Smalley <john.m.smalley@gmail.com>
2016-09-28 17:30:10 -0700

    submission page auto generators categories and subcategories

--- client/src/components/CategoryList.js
+++ client/src/components/CategoryList.js
@@ -7,14 +7,32 @@ export default class CategoryList extends Component {
   }
 
   render() {
-    console.log('----Props------', this.props);
-    console.log('list: ', list);
+    var that = this;
     return (
       <div>
         <h1>Inside the CategoryList</h1>
         <div className="panel-group" id="accordion">
-          {list.map(function(category) {
-            <h3>{category[0]}</h3>
+          {list.map(function(category, catIndex) {
+            return (
+              <div className="panel panel-default">
+                <div className="panel-heading">
+                  <h4 className="panel-title" data-toggle="collapse" data-parent="#accordion" href={"#collapse" + catIndex}>
+                    {category[0]}
+                  </h4>
+                </div>
+                <div id={"collapse" + catIndex} className="panel-collapse collapse">
+                  {category.map(function(subCategory, subIndex) {
+                    if (subIndex > 0) {
+                      return (
+                        <div className="panel-body" className="list-group">
+                           <button type="button" className="list-group-item btn-xs" onClick={that.props.selectCategory.bind(that,category[0],subCategory)}>{subCategory}</button>
+                        </div>
+                      )
+                    }
+                  })}
+                </div>
+              </div>
+            )
           })}
         </div>
       </div>
@@ -22,3 +40,4 @@ export default class CategoryList extends Component {
   }
 
 }
+
