418873e078b081eff2d3645f2afa4edd699166d9
John Smalley <john.m.smalley@gmail.com>
2016-10-03 16:14:29 -0700

    added confirm option

--- client/src/components/admin/UserEntry.jsx
+++ client/src/components/admin/UserEntry.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Modal, OverlayTrigger, Popover, Tooltip, Button } from 'react-bootstrap';
 
 export default class UserEntry extends React.Component {
   constructor(props) {
@@ -7,6 +8,16 @@ export default class UserEntry extends React.Component {
     this.state = {};
     this.state.view = 'normal';
     this.state.type;
+    this.state.showModal = false;
+  }
+
+
+  close() {
+    this.setState({ showModal: false });
+  }
+
+  open() {
+    this.setState({ showModal: true });
   }
 
   renderUser() {
@@ -16,9 +27,10 @@ export default class UserEntry extends React.Component {
         <td>{this.props.user.username}</td>,
         <td>{this.props.user.email}</td>,
         <td>{this.props.user.type}</td>,
+        <td>{this.props.user.froozen}</td>,
         <td>
           <button onClick={this.changeView.bind(this, 'edit')} type="button" className="btn btn-success btn-xs">Edit</button>
-          <button type="button" className="btn btn-danger btn-xs">Delete</button>
+          <button onClick={this.open.bind(this)} type="button" className="btn btn-danger btn-xs">Delete</button>
         </td>,
         ]
       )
@@ -35,12 +47,13 @@ export default class UserEntry extends React.Component {
             onChange={ event => this.setState({type: event.target.value})}>
           </input>
         </td>,
+        <td>{this.props.user.froozen}</td>,
         <td>
           {this.state.type > 0 && this.state.type < 4 ? <button
             type="button"
             className="btn btn-success btn-xs"
             onClick={this.saveUser.bind(this)}>Save</button> : null}
-          <button type="button" className="btn btn-danger btn-xs">Delete</button>
+          <button onClick={this.open.bind(this)} type="button" className="btn btn-danger btn-xs">Delete</button>
         </td>,
         ]
       )
@@ -52,14 +65,12 @@ export default class UserEntry extends React.Component {
   }
 
   saveUser() {
-    console.log('state: ', this.state);
-    console.log('user: ', this.props.user);
     $.ajax({
       url: '/users/' + this.props.user.id,
       method: 'PUT',
       data: JSON.stringify({type: this.state.type})
     })
-    .done((users) => {
+    .done((user) => {
       this.props.user.type = this.state.type;
       this.setState({view: 'normal'});
     })
@@ -68,10 +79,39 @@ export default class UserEntry extends React.Component {
     })
   }
 
+  freezeUser() {
+    $.ajax({
+      url: '/users/' + this.props.user.id,
+      method: 'PUT',
+      data: JSON.stringify({froozen: true})
+    })
+    .done((user) => {
+      this.props.user.username = 'deleted';
+      this.props.user.email = 'deleted';
+      this.props.user.type = 'deleted';
+      this.close.bind(this);
+    }
+  }
+
   render() {
+
     return (
       <tr>
         {this.renderUser()}
+
+        <Modal show={this.state.showModal} onHide={this.close.bind(this)}>
+          <Modal.Header closeButton>
+            <Modal.Title>Confirm Freeze A User</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h3>Are you sure you want to freeze this user?</h3>
+            <p>A froozen user will not be allowed to log back into this site until they are unfroozen again</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.close.bind(this)}>Cancel</Button>
+            <Button bsStyle="danger" onClick={this.close.bind(this)}>Yes Freeze User</Button>
+          </Modal.Footer>
+        </Modal>
       </tr>
     )
   }
