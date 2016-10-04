1ec5f94195362179580a57c175aad2ac9be547cf
Anna <annatangzhao@gmail.com>
2016-10-04 15:16:41 -0700

    changes

--- client/src/index.js
+++ client/src/index.js
@@ -21,7 +21,7 @@ import FlaggedVideos from './components/admin/FlaggedVideos';
 import AdminControls from './components/admin/AdminControls';
 import NeedSuperUser from './components/NeedSuperUser';
 import NeedAdminUser from './components/NeedAdminUser';
-import Communities from './components/communities/CRecords'; //needs to be changed
+import Community from './components/community/CRecords'; //needs to be changed
 import { AUTH_USER, UNAUTH_USER } from './actions/types';
 import {RequiresTypeOne, RequiresTypeTwo, RequiresTypeThree} from './components/auth/require_auth';
 
