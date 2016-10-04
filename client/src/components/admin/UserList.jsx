686976f7da5cf0c006d00984b8beb9b9f7c0dd5d
Jim Yang <jim@sidefra.me>
2016-10-03 23:10:51 -0400

    Update UserList.jsx

--- client/src/components/admin/UserList.jsx
+++ client/src/components/admin/UserList.jsx
@@ -18,7 +18,7 @@ export default class UserList extends React.Component {
                   <th>UserName</th>
                   <th>Email</th>
                   <th>User Type</th>
-                  <th>Froozen</th>
+                  <th>Frozen</th>
                   <th>Options</th>
                 </tr>
               </thead>
