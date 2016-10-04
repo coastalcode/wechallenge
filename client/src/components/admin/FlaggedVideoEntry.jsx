c2a1819603890742c268f73fbf35652a84dca605
John Smalley <john.m.smalley@gmail.com>
2016-10-04 13:46:43 -0700

    added delete video ability

--- client/src/components/admin/FlaggedVideoEntry.jsx
+++ client/src/components/admin/FlaggedVideoEntry.jsx
@@ -1,33 +1,38 @@
 import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
 
 export default class RecordEntry extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-    }
+    this.state = {}
+    this.state.showDeleteModal = false;
   }
 
-  adminRemoveVideo () {
+  showDeleteModal() {
+    this.setState({ showDeleteModal: true });
+  }
+
+  hideDeleteModal() {
+    this.setState({ showDeleteModal: false });
+  }
+
+  deleteSubmission () {
     console.log(this.props.flag.id)
     let init = {
-      method: 'GET',
-      headers: new Headers()
+      method: 'DELETE'
     }
 
-    fetch(`/submissions/adminremove/${ this.props.flag.id }`)
+    fetch(`/submissions/${ this.props.flag.id }`, init)
       .then((removed)=>{
-        console.log(this.props.flag.id)
-        console.log("Bye!!")
+        console.log(this.props.flag.id);
+        console.log("Bye!!");
+        this.hideDeleteModal();
     })
   }
 
   toggleFlagVideo () {
     console.log(this.props.flag.id)
-    let init = {
-      method: 'GET',
-      headers: new Headers()
-    }
 
     fetch(`/submissions/flag/${ this.props.flag.id }`)
       .then((flagged)=>{
@@ -39,12 +44,28 @@ export default class RecordEntry extends React.Component {
     let url = `https://www.youtube.com/embed/${ this.props.flag.link }`
     return (
       <div>
+
         { this.props.flag.title }
         <br/>
         <iframe width="560" height="315" src={ url } frameBorder="0" allowFullScreen></iframe>
         <br />
-        <button onClick={event=>{ this.adminRemoveVideo() }} >Remove this video</button>
+        <button onClick={this.showDeleteModal.bind(this)} >Remove this video</button>
         <button onClick={event=>{ this.toggleFlagVideo()  }} >Unflag this video</button>
+
+        <Modal show={this.state.showDeleteModal} onHide={this.hideDeleteModal.bind(this)}>
+          <Modal.Header closeButton>
+            <Modal.Title>Confirm Deleting a Submission</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h3>Are you sure you want to delete this submission?</h3>
+            <p>A all of the content assoicated to this submission like comments, votes, community comments will also be deleted.</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.hideDeleteModal.bind(this)}>Cancel</Button>
+            <Button bsStyle="danger" onClick={this.deleteSubmission.bind(this)}>Yes Delete Submission</Button>
+          </Modal.Footer>
+        </Modal>
+
       </div>
     )
   }
